51c59da1d0ff7f64f54b9838528fb0aeb7902738
Valerie S <43964677+vs0018@users.noreply.github.com>
2018-12-15 18:22:43 -0500

    finalize button logic and DOM manipulation

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,37 @@
+$(document).ready(function(){
+
 var topics = ["Christmas", "4th of July", "Halloween"];
 
+function renderButtons() {
+    $("#button-dump").empty();
+
+    for (var i = 0; i < topics.length; i++) {
+        var a = $("<button>");
+        a.addClass("holiday");
+        a.attr("data-name", topics[i]);
+        a.text(topics[i]);
+        $("#button-dump").append(a);
+    }
+  };
+
 $("#add").on("click", function(){
-    var newItem = $("#newItem").val();
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      newItem + "&api_key=Y8jq6XrPYojVO006xsPEuYBovosMxYYZ&limit=10";
+
+    var newItem = $("#newItem").val().trim();
+
+    topics.push(newItem);
+    
+    renderButtons();
+    });
+
+$(document).on("click", ".holiday", function(event){
+    $("#gif-dump").empty();
+    
+    var buttonName = $(this).attr("data-name");
+
+    console.log(buttonName);
+
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+      buttonName + "&api_key=Y8jq6XrPYojVO006xsPEuYBovosMxYYZ&limit=10";
 
     $.ajax({
       url: queryURL,
@@ -16,24 +44,19 @@ $("#add").on("click", function(){
     
       for (var i = 0; i < results.length; i++) {
 
-      // Make a div with jQuery and store it in a variable named animalDiv.
-      var gifDiv = $("<div>");
-      // Make a paragraph tag with jQuery and store it in a variable named p.
-      var p = $("<p>");
-      // Set the inner text of the paragraph to the rating of the image in results[i].
-      p.text(results[i].rating);
-      // Make an image tag with jQuery and store it in a variable named animalImage.
-      var gifImage = $("<img>");
-      // Set the image's src to results[i]'s fixed_height.url.
-      gifImage.attr("src", results[i].images.fixed_height.url);
-      // Append the p variable to the animalDiv variable.
-      $(gifDiv).append(p);
-      // Append the animalImage variable to the animalDiv variable.
-      $(gifDiv).append(gifImage);
-      // Prepend the animalDiv variable to the element with an id of gifs-appear-here.
-      $("#gifs-dump").prepend(gifDiv);
-      }
+        var gifDiv = $("<div>");
+        var p = $("<p>");
+    
+        p.text("Rating: " + results[i].rating);
+    
+        var gifImage = $("<img>");
 
+        gifImage.attr("src", results[i].images.fixed_height.url);
+    
+        $(gifDiv).append(p);
+        $(gifDiv).append(gifImage);
+        $("#gif-dump").prepend(gifDiv);
+      }
     });
 });
 
@@ -53,4 +76,8 @@ $(".gif").on("click", function() {
             $(this).attr("src", still);
             $(this).attr("data-state", "still");
         };
+});
+
+renderButtons();
+
 });
