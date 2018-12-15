08bb783faaa87b45b446d9bfae3e1df551635830
Valerie S <43964677+vs0018@users.noreply.github.com>
2018-12-15 16:28:05 -0500

    add start/stop logic

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,25 @@
 var topics = ["Christmas", "4th of July", "Halloween"];
 
+$("#add").on("click", function(){
+
+
+
+
+
+});
+
+//to start & pause gifs
+var state = $(this).attr("data-state")
+
+      if (state === "still") {
+        var animate = $(this).attr("data-animate");
+
+        $(this).attr("src", animate);
+        $(this).attr("data-state", "animate");
+
+      } else if (state === "animate") {
+        var still = $(this).attr("data-still");
+
+        $(this).attr("src", still);
+        $(this).attr("data-state", "still");
+      };
